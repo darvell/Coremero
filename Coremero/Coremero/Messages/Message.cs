92821ec77042bd8101133b8b290636631fac9320
Darvell Long <contact@darvell.me>
2017-01-25 10:35:06 -0800

    Implement standard messages.

---
+++ Coremero/Coremero/Messages/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coremero.Messages
+{
+    public class Message : IMessage
+    {
+        public string Text { get; set; }
+        public List<IAttachment> Attachments { get; set; }
+
+        public static Message Create(string text, params IAttachment[] attachments)
+        {
+            Message message = new Message()
+            {
+                Text = text,
+                Attachments =  attachments?.ToList()
+            };
+            return message;
+        }
+
+        public static Message Create(string text)
+        {
+            return Message.Create(text, null);
+        }
+
+    }
+}
