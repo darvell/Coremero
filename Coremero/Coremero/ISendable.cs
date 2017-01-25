a99ddfc7cc25249ac218127088eb0f0a62600cdc
Darvell Long <contact@darvell.me>
2017-01-25 10:30:40 -0800

    ISendable interface.

--- Coremero/Coremero/ISendable.cs
+++ Coremero/Coremero/ISendable.cs
@@ -1,6 +1,20 @@
-﻿namespace Coremero
+﻿using System.Threading.Tasks;
+
+namespace Coremero
 {
     public interface ISendable
     {
+        /// <summary>
+        /// Sends a message to the target asynchronously and can be awaited on until the server confirms that the message has been handled successfully.
+        /// </summary>
+        /// <param name="message">The message to be sent.</param>
+        /// <returns>Awaitable task.</returns>
+        Task SendAsync(IMessage message);
+
+        /// <summary>
+        /// Sends a message to the target in a fire-and-forget process.
+        /// </summary>
+        /// <param name="message">The message to be sent.</param>
+        void Send(IMessage message);
     }
 }
