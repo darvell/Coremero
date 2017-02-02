5e53b36f2cd49f1626a809bcb6eaecbcf67f5c43
Darvell Long <contact@darvell.me>
2017-02-01 16:33:00 -0800

    reactions

--- Coremero/Coremero.Client.Discord/DiscordMessage.cs
+++ Coremero/Coremero.Client.Discord/DiscordMessage.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Rest;
 using Discord.Rpc;
 
 namespace Coremero.Client.Discord
@@ -26,6 +29,26 @@ namespace Coremero.Client.Discord
                 // TODO: Convert attachments.
                 return null;
             }
+        }
+
+        public async Task React(string emoji)
+        {
+            var restMessage = (RestUserMessage) await _message.Channel.GetMessageAsync(_message.Id);
+            await restMessage.AddReactionAsync(emoji);
+        }
+
+        public async Task<List<string>> GetReactions()
+        {
+            var restMessage = (RestUserMessage) await _message.Channel.GetMessageAsync(_message.Id);
+            List<string> result = new List<string>();
+            foreach (KeyValuePair<Emoji, int> reaction in restMessage.Reactions)
+            {
+                for (int i = 0; i < reaction.Value; i++)
+                {
+                    result.Add(reaction.Key.ToString());
+                }
+            }
+            return result;
         }
     }
 }
