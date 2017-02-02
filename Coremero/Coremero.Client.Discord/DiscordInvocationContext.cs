4a17bdec1c4579ba3adc3e817a127eaf2c2335eb
Darvell Long <contact@darvell.me>
2017-02-01 16:33:20 -0800

    start on discord invocation context

--- Coremero/Coremero.Client.Discord/DiscordInvocationContext.cs
+++ Coremero/Coremero.Client.Discord/DiscordInvocationContext.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Coremero.Client.Discord
 {
-    class DiscordInvocationContext
+    public class DiscordInvocationContext : IInvocationContext
     {
+        public IClient OriginClient { get; private set; }
+        public ISendable Raiser { get; }
+        public IUser User { get; }
+        public IChannel Channel { get; }
+
+        public DiscordInvocationContext(IClient client)
+        {
+            OriginClient = client;
+        }
+
     }
 }
