d686c9eba6e8ed1c309255534810aef94bbcebca
Darvell Long <contact@darvell.me>
2017-02-01 19:07:24 -0800

    Assembly scanning, singleton registration/injection

--- Coremero/Coremero/Coremero.cs
+++ Coremero/Coremero/Coremero.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 using System.Text;
+using Coremero.Client;
 using Coremero.Registry;
 using Coremero.Services;
+using Microsoft.Extensions.PlatformAbstractions;
 using SimpleInjector;
+using SimpleInjector.Advanced;
 
 namespace Coremero
 {
@@ -11,6 +18,7 @@ namespace Coremero
     {
         private static bool _hasInit;
         private static Container _container;
+
         public static void Initialize()
         {
             if (_hasInit)
@@ -19,6 +27,7 @@ namespace Coremero
             }
 
             _container = new Container();
+            _container.Options.LifestyleSelectionBehavior = new SingletonLifestyleSelectionBehavior(); // Lazy hack to force all plugins to be singleton.
 
             // Register registries
             _container.RegisterSingleton<ClientRegistry>();
@@ -27,13 +36,34 @@ namespace Coremero
             // Register services
             _container.RegisterSingleton<IMessageBus, MessageBus>();
             _container.RegisterSingleton<ICommandHandler, CommandHandler>();
-            
 
+            // Scan for clients
+            var clientAssemblies =
+                from file in new DirectoryInfo(PlatformServices.Default.Application.ApplicationBasePath).GetFiles()
+                where file.Extension.ToLower() == ".dll" && file.Name.StartsWith("Coremero.Client.")
+                select AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+            _container.RegisterCollection<IClient>(clientAssemblies);
 
+            // Scan for plugins
+            string pluginDirectory = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "Plugins");
+            var pluginAssemblies = 
+                from file in new DirectoryInfo(pluginDirectory).GetFiles()
+                where file.Extension.ToLower() == ".dll" && file.Name.StartsWith("Coremero.Plugin.")
+                select AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+
+            _container.RegisterCollection<IPlugin>(pluginAssemblies);
 
             _container.Verify();            
 
             _hasInit = true;
+        }
+
+        public class SingletonLifestyleSelectionBehavior : ILifestyleSelectionBehavior
+        {
+            public Lifestyle SelectLifestyle(Type serviceType, Type implementationType)
+            {
+                return Lifestyle.Singleton;
+            }
         }
 
     }
