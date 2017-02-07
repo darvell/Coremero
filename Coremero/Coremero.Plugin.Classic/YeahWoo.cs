7adfdcecf313ac22a5afb07bb53ad1cf299b8b94
Darvell Long <contact@darvell.me>
2017-02-07 15:41:12 -0800

    Yeah! Woo!

---
+++ Coremero/Coremero.Plugin.Classic/YeahWoo.cs
@@ -0,0 +1,25 @@
+﻿using Coremero.Messages;
+using Coremero.Services;
+
+namespace Coremero.Plugin.Classic
+{
+    public class YeahWoo : IPlugin
+    {
+        public YeahWoo(IMessageBus messageBus)
+        {
+            messageBus.Received += MessageBus_Received;
+        }
+
+        private async void MessageBus_Received(object sender, MessageReceivedEventArgs e)
+        {
+            if (e.Message.Text.ToLower().Equals("yeah"))
+            {
+                await e.Context.Raiser.SendAsync(Message.Create("Woo!"));
+            }
+            else if (e.Message.Text.ToLower().Equals("woo"))
+            {
+                await e.Context.Raiser.SendAsync(Message.Create("Yeah!"));
+            }
+        }
+    }
+}
