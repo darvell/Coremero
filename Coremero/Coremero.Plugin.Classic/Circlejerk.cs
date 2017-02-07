9ec97bc105f74977667e6540c0e7077d458bfc0f
Darvell Long <contact@darvell.me>
2017-02-07 15:47:08 -0800

    Move from per-user jokes to a single circlejerk plugin. Added depths.

--- Coremero/Coremero.Plugin.Classic/Circlejerk.cs
+++ Coremero/Coremero.Plugin.Classic/Circlejerk.cs
@@ -1,13 +1,20 @@
 ﻿using Coremero.Commands;
+using Coremero.Utilities;
 
 namespace Coremero.Plugin.Classic
 {
-    public class Clump : IPlugin
+    public class Circlejerk : IPlugin
     {
         [Command("clump")]
         public string BsFeelings(IInvocationContext context, IMessage message)
         {
             return "゜・。。・゜゜・。。・゜☆゜・。。・゜ im too bullshit feeligns  。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜";
+        }
+
+        [Command("depths")]
+        public string Depths(IInvocationContext context, IMessage message)
+        {
+            return message.Text.TrimCommand().Replace('o', 'ø');
         }
     }
 }
