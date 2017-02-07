25573277b7268b667810097b67893a0a9d4b28d9
Darvell Long <contact@darvell.me>
2017-02-07 11:15:25 -0800

    IClient xmldoc

--- Coremero/Coremero/Client/IClient.cs
+++ Coremero/Coremero/Client/IClient.cs
@@ -6,16 +6,36 @@ namespace Coremero.Client
 {
     public interface IClient
     {
+        /// <summary>
+        /// The name of the client, generally the supported protocol. (e.g. IRC, Discord, et al.)
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// A description of the client.
+        /// </summary>
         string Description { get; }
 
+        /// <summary>
+        /// What features the client supports.
+        /// </summary>
         ClientFeature Features { get; }
 
+        /// <summary>
+        /// Returns true if the client is connected to the appropriate backend, else false.
+        /// </summary>
         bool IsConnected { get; }
 
+        /// <summary>
+        /// Connects a client asynchronously.
+        /// </summary>
+        /// <returns>Task for the connection process.</returns>
         Task Connect();
 
+        /// <summary>
+        /// Disconnects a client asynchronously, throws an exception if not connected.
+        /// </summary>
+        /// <returns>Task for the disconnection process.</returns>
         Task Disconnect();
 
         /// <summary>
@@ -23,11 +43,6 @@ namespace Coremero.Client
         /// </summary>
         event EventHandler<Exception> Error;
 
-
         IEnumerable<IServer> Servers { get; }
-
-        //event EventHandler<MessageEventArgs> MessageReceived;
-
-        //event EventHandler<MessageEventArgs> MessageSent;
     }
 }
