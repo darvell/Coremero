e5f8b3d7f04c81486bce22f7fc669d3dbb60ed5d
Darvell Long <contact@darvell.me>
2017-02-07 15:43:52 -0800

    He's too BS feelings.

---
+++ Coremero/Coremero.Plugin.Classic/Clump.cs
@@ -0,0 +1,13 @@
+﻿using Coremero.Commands;
+
+namespace Coremero.Plugin.Classic
+{
+    public class Clump : IPlugin
+    {
+        [Command("clump")]
+        public string BsFeelings(IInvocationContext context, IMessage message)
+        {
+            return "゜・。。・゜゜・。。・゜☆゜・。。・゜ im too bullshit feeligns  。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜";
+        }
+    }
+}
