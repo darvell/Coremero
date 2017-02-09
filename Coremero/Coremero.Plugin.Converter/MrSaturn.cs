185323c674934d628ab5408164c2cf77acf522f5
Darvell Long <contact@darvell.me>
2017-02-08 18:24:40 -0800

    Mr. Saturn

---
+++ Coremero/Coremero.Plugin.Converter/MrSaturn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Coremero.Commands;
+using Coremero.Utilities;
+
+namespace Coremero.Plugin.Converter
+{
+    public class MrSaturn : IPlugin
+    {
+        private Dictionary<char, char> _saturnReplacements = new Dictionary<char, char>
+        {
+            {'a', 'ᗩ'},
+            {'b', 'ᗷ'},
+            {'c', 'ᘓ'},
+            {'d', 'ᗪ'},
+            {'e', 'ᕮ'},
+            {'f', 'ᖴ'},
+            {'g', 'ᕤ'},
+            {'h', 'ᗁ'},
+            {'i', 'ᓮ'},
+            {'j', 'ᒎ'},
+            {'k', 'ᔌ'},
+            {'l', 'ᒪ'},
+            {'m', 'ᙏ'},
+            {'n', 'ᑎ'},
+            {'o', 'ᘎ'},
+            {'p', 'ᖘ'},
+            {'q', 'ᕴ'},
+            {'r', 'ᖇ'},
+            {'s', 'ᔕ'},
+            {'t', 'ᒮ'},
+            {'u', 'ᘮ'},
+            {'v', 'ᐯ'},
+            {'w', 'ᙎ'},
+            {'x', '᙭'},
+            {'y', 'ᖿ'},
+            {'z', 'ᔓ'},
+            {'\'', 'ᐞ'}
+        };
+
+        [Command("saturn")]
+        public string SaturnConvert(IInvocationContext context, IMessage message)
+        {
+            return String.Concat(message.Text?.TrimCommand().Select(x =>
+            {
+                if (_saturnReplacements.ContainsKey(x))
+                {
+                    return _saturnReplacements[x];
+                }
+                return x;
+            }) ?? String.Empty);
+        }
+
+    }
+}
