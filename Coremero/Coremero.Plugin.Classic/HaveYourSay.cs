9f7327cafd93df7eacf639aaf3d422f58247c2b4
Darvell Long <contact@darvell.me>
2017-02-10 12:02:12 -0800

    Missing GetRandom in HYS.

--- Coremero/Coremero.Plugin.Classic/HaveYourSay.cs
+++ Coremero/Coremero.Plugin.Classic/HaveYourSay.cs
@@ -62,7 +62,7 @@ namespace Coremero.Plugin.Classic
                 builder.Append($" because {_hysData["because"].GetRandom()}");
             }
 
-            builder.Append($"{GenerateRandomIdiotPunctuation()}{_hysData["imperative"]} {_hysData["moronic_solution"].GetRandom()}{GenerateRandomIdiotPunctuation()}");
+            builder.Append($"{GenerateRandomIdiotPunctuation()}{_hysData["imperative"].GetRandom()} {_hysData["moronic_solution"].GetRandom()}{GenerateRandomIdiotPunctuation()}");
 
             if (_rnd.NextDouble() > 0.4)
             {
