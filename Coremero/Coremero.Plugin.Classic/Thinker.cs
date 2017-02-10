451de50fbbd9232c38ea00462a346535eb336d4f
Darvell Long <contact@darvell.me>
2017-02-10 01:12:14 -0800

    Think

---
+++ Coremero/Coremero.Plugin.Classic/Thinker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Coremero.Client;
+using Coremero.Commands;
+using Coremero.Utilities;
+
+namespace Coremero.Plugin.Classic
+{
+    public class Thinker : IPlugin
+    {
+        [Command("think", "Thought", Help = "( .   __ . ) . o O ( [Thought] )")]
+        public string Think(IInvocationContext context, IMessage message)
+        {
+            string output = $"( .   __ . ) . o O ( {message.Text.TrimCommand()} )";
+
+            if (context.OriginClient.Features.HasFlag(ClientFeature.Markdown))
+            {
+                output = output.PadLeft(3, '`');
+                output = output.PadRight(3, '`');
+            }
+            return output;
+        }
+    }
+}
