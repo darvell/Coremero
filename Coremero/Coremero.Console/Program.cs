8925487e8480e168767fb5971d66111a40ef23cd
Darvell Long <contact@darvell.me>
2017-02-12 15:54:17 -0800

    Don't close unless Ctrl-C instead of ReadLine().

--- Coremero/Coremero.Console/Program.cs
+++ Coremero/Coremero.Console/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Threading;
+
 class Program
 {
     static void Main(string[] args)
     {
+        var exitEvent = new ManualResetEvent(false);
+
+        Console.CancelKeyPress += (sender, eventArgs) => {
+            eventArgs.Cancel = true;
+            exitEvent.Set();
+        };
+
         Coremero.Coremero.Initialize();
-        Console.ReadLine();
+        exitEvent.WaitOne();
     }
 }
