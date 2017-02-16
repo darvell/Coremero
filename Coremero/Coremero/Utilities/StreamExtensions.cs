2db2f98c07e2d24ae068978268c83e84af95bc1d
Darvell Long <contact@darvell.me>
2017-02-16 11:37:27 -0800

    Stream extensions.

---
+++ Coremero/Coremero/Utilities/StreamExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coremero.Utilities
+{
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Reads and copies a stream to a MemoryStream asynchronously.
+        /// </summary>
+        /// <param name="stream">Input stream.</param>
+        /// <param name="disposeOriginalStream">Disposes the original stream after copying if true.</param>
+        public static async Task<MemoryStream> CopyToMemoryStreamAsync(this Stream stream, bool disposeOriginalStream = true)
+        {
+            MemoryStream result = new MemoryStream();
+            await stream.CopyToAsync(result);
+
+            if (disposeOriginalStream)
+            {
+                stream.Dispose();
+            }
+
+            // Seek to start like it's brand new.
+            result.Seek(0, SeekOrigin.Begin);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Reads and copies a stream to a MemoryStream.
+        /// </summary>
+        /// <param name="stream">Input stream.</param>
+        /// <param name="disposeOriginalStream">Disposes the original stream after copying if true.</param>
+        public static MemoryStream CopyToMemoryStream(this Stream stream, bool disposeOriginalStream = true)
+        {
+            return CopyToMemoryStreamAsync(stream, disposeOriginalStream).Result;
+        }
+    }
+}
