0c6e136ac93b5c83f0aff97b336692f92b3a059c
Darvell Long <contact@darvell.me>
2017-02-17 15:24:00 -0800

    URL content snatch command.

---
+++ Coremero/Coremero.Plugin.Playground/ImageSnatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Coremero.Commands;
+using Coremero.Messages;
+using Coremero.Utilities;
+
+namespace Coremero.Plugin.Playground
+{
+    public class ImageSnatch
+    {
+        [Command("geturl")]
+        public async Task<IMessage> GetUrl(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var result = await client.GetAsync(url);
+                if (result.IsSuccessStatusCode && result.Content.Headers.ContentLength < 6000000)
+                {
+                    return Message.Create(null,
+                        new StreamAttachment(await (await result.Content.ReadAsStreamAsync()).CopyToMemoryStreamAsync(), Path.GetFileName(url)));
+                }
+            }
+
+            throw new Exception("Can't get content.");
+        }
+    }
+}
