a1d6cdbd3a255594d68cd4e7ad5195abcc6a1c5d
Darvell Long <contact@darvell.me>
2017-02-24 14:34:14 -0800

    Debug user status

---
+++ Coremero/Coremero.Plugin.Playground/Debug.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Coremero.Client;
+
+namespace Coremero.Plugin.Playground
+{
+    public class Debug : IPlugin
+    {
+        private readonly IClientUserStatus _client;
+        private long _minutesAlive = 0;
+        public Debug(IEnumerable<IClient> clients)
+        {
+            _client = clients.FirstOrDefault(x => x is IClientUserStatus) as IClientUserStatus;
+
+            var timer = new Timer((state) =>
+            {
+                _minutesAlive += 1;
+                if (_client != null)
+                {
+                    _client.UserStatus = $"U: {_minutesAlive}m | A: {GC.GetTotalMemory(false) / 1024.0}MB ";
+                }
+            }, null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
+        }
+
+    }
+}
