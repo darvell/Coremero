a83aac05b738b12c2714117b244ad44235e3a9f4
Darvell Long <contact@darvell.me>
2017-05-13 14:36:25 -0700

    don't flood errors

--- Coremero/Coremero.Plugin.Classic/Autoreact.cs
+++ Coremero/Coremero.Plugin.Classic/Autoreact.cs
@@ -43,6 +43,7 @@ namespace Coremero.Plugin.Classic
                     catch
                     {
                         // Whatever
+                        break;
                     }
                 }
             }
