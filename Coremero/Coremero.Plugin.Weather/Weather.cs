a6a5d471782f3b037bfc73ae5b6dfe84af672bce
Darvell Long <contact@darvell.me>
2018-06-01 15:05:36 -0700

    michael is my best friend again and i appreciate the work he does i take back all the commits

--- Coremero/Coremero.Plugin.Weather/Weather.cs
+++ Coremero/Coremero.Plugin.Weather/Weather.cs
@@ -300,7 +300,7 @@ namespace Coremero.Plugin.Weather
                             VerticalAlignment = VerticalAlignment.Center,
                             IsRelative = true,
                             X = -50,
-                            Y = 60,
+                            Y = 65,
                             Font = mdFont,
                             Color = WeatherColors.TealAlso
                         },
